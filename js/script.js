1624b55d4e95eeae29ff9ba68613fcf728cf89c7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-14 22:36:25 -0300

    gabs

--- js/script.js
+++ js/script.js
@@ -1,47 +1,66 @@
-function ValidarCampos(){
-    let nome = document.getElementById("nome");
-    let email = document.getElementById("email");
-
-
-    if(nome.value == ""){
-        alert("Digite o nome!");
-        nome.focus();
-        return;
+let chave = "Nome 1";
+let chave2 = "Curso 1";
+let chave3 = "Ano";
+let i = 0;
+ 
+function ArmazenarDados()
+{
+    localStorage.setItem(chave, document.getElementById("nome").value);
+    localStorage.setItem(chave2, document.getElementById("curso").value);
+    localStorage.setItem(chave3, document.getElementById("ano").value);
+    LimparCampos();
 }
-else{
-    if(nome.value.length > 20){
-        alert("Limite de caracteres 20 caracteres ultrapassado.");
-        nome.value = "";
-        nome.focus();
-        return;
-    }
+ 
+function ConsultarDados()
+{
+    document.getElementById("resultado").innerHTML = localStorage.getItem(chave) + " - " +
+    localStorage.getItem(chave2);
 }
-    if(email.value ==""){
-        alert("Digite o email!");
-        email.focus();
-        return;
-    }
-    else
+ 
+// Function para limpar dados após a utilização do botão
+ 
+function LimparCampos()
+{
+    document.getElementById("nome").value = "";
+    document.getElementById("curso").value = "";
+    document.getElementById("ano").value = "";
+}
+ 
+function AtualizarDados()
+{
+    localStorage.setItem(chave, document.getElementById("nome").value);
+    localStorage.setItem(chave2, document.getElementById("curso").value);
+    localStorage.setItem(chave3, document.getElementById("ano").value);
+    LimparCampos();
+}
+ 
+function insertData()
+{  
+    i = i + 1;
+    localStorage.setItem("Nome " + i , document.getElementById("nome").value);
+    localStorage.setItem("Curso " + i, document.getElementById("curso").value);
+    localStorage.setItem("ano " + i, document.getElementById("ano").value);
+    LimparCampos();
+}
+ 
+function ViewAllData()
+{
+    let resultado = "";
+    for(let indice = 1; indice <= i; indice++)
     {
-        if(!ValidaEmail(email.value.trim())){
-            alert("Email Incorreto!");
-            email.value = "";
-            email.focus();
-            return;
-        }    
-        else
-        {
-
-            //let parteEmail = email.value.substring(0,5);
-            //alert(parteEmail);
-
-            let antesDoArroba = email.value.substring(0, email.value.indexOf("@"));
-            alert(antesDoArroba);
-        }
+        resultado += localStorage.getItem("Nome " + indice) + " - " +  
+        localStorage.getItem("Curso " + indice ) + " - " + localStorage.getItem("ano " + indice );
+        "<br>";
     }
+ 
+    document.getElementById("resultado").innerHTML = resultado;
 }
-
-function ValidaEmail(email){
-    const regex = /\S+@\S+\.\S+/;
-    return regex.test(email);
+ 
+function DeleteData()
+{
+    localStorage.removeItem("Nome " + document.getElementById("ano").value);
+    localStorage.removeItem("Curso " + document.getElementById("curso").value);
+    localStorage.removeItem("ano " + document.getElementById("ano").value);
+   
 }
+ 
